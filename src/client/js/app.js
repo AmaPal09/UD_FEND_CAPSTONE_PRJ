ad05abeead6bec7af6fca4804403f17c1d93ce59
Amarja Palekar <amarjapalekar09@gmail.com>
2021-11-28 23:27:11 -0800

    FEAT: Validate user input forms

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,4 +1,51 @@
-// Variables
-const tripDestination = document.getElementById('tripDestination');
-const tripStartDate = document.getElementById('tripStartDate');
+// VARIABLES
 
+// Variables for user input
+
+
+const generatePlan = document.getElementById('generatePlan');
+
+
+//Functions
+function validateInput(destination, date){
+	const result = {
+		valid: false,
+		msg: "Enter a destination and date"};
+
+
+	if (destination == "" || date == "") {
+		if(destination == ""){
+			result.msg = "Enter a destination";
+			return result;
+		}
+		else if(date == ""){
+			result.msg = "Select a date";
+			return result;
+		}
+	}
+	else {
+		result['valid'] = true;
+		result['msg'] = 'All OK';
+		return result
+	}
+}
+
+
+function submitTripInfoForm(e){
+	e.preventDefault();
+
+	const tripDestination = document.getElementById('tripDestination').value;
+	const tripStartDate = document.getElementById('tripStartDate').value;
+
+	const result = validateInput(tripDestination, tripStartDate);
+	if (result.valid) {
+		console.log("All inputs okay")
+		// TODO: process the input
+	}
+	else{
+		console.log(result.msg);
+	}
+}
+
+//Event Listners
+generatePlan.addEventListener('click', submitTripInfoForm);
