3bf21ed83273469e7cba87114f2412f6257aed78
Amarja Palekar <amarjapalekar09@gmail.com>
2021-12-10 18:52:54 -0800

    FEAT: Add date validation to ensure trip date it bigger than the current Date

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -7,19 +7,19 @@ const generatePlan = document.getElementById('generatePlan');
 
 
 //Functions
-function validateInput(destination, date){
-	const result = {
-		valid: false,
-		msg: "Enter a destination and date"};
+function validateInputs(destination, date){
+	const result = {};
 
 
 	if (destination == "" || date == "") {
 		if(destination == ""){
 			result.msg = "Enter a destination";
+			result.valid = false;
 			return result;
 		}
 		else if(date == ""){
 			result.msg = "Select a date";
+			result.valid = false;
 			return result;
 		}
 	}
@@ -31,16 +31,40 @@ function validateInput(destination, date){
 }
 
 
+function validateFutureDate(fDate, cDate) {
+	console.log(fDate);
+	console.log(cDate);
+	if (fDate.getTime() > cDate.getTime()){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
 function submitTripInfoForm(e){
 	e.preventDefault();
 
 	const tripDestination = document.getElementById('tripDestination').value;
 	const tripStartDate = document.getElementById('tripStartDate').value;
 
-	const result = validateInput(tripDestination, tripStartDate);
-	if (result.valid) {
-		console.log("All inputs okay")
-		// TODO: process the input
+	const inputsPresent = validateInputs(tripDestination, tripStartDate);
+	if (inputsPresent.valid) {
+		console.log(inputsPresent.msg);
+
+		//Validate trip start date is bigger the current Date
+		// console.log(tripStartDate);
+		const currDate = new Date();
+		let futureDate = new Date(tripStartDate+'T00:00');//convert to date in current time zone
+
+		const validDepartureDate = validateFutureDate(futureDate, currDate);
+		if (validDepartureDate) {
+			console.log('Trip start date is bigger than current Date');
+		}
+		else {
+			console.log("Something went wrong")
+		}
 	}
 	else{
 		console.log(result.msg);
