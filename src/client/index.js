01b3a51a34f66b70f8daaab1bccd53085f49ec34
Amarja Palekar <amarjapalekar09@gmail.com>
2022-01-05 14:24:09 -0800

    CHNG: Load everything after DOM

--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,19 @@
 // Webpack entry file
 
 //import styles
-import './styles/main.scss'
+import (/*webpackPreload: true*/'./styles/main.scss');
 
 // import js scripts
 import { addEvents, submitTripInfoForm } from './js/app.js';
 // import { submitTripInfoForm } from './js/app.js';
 
+// console.log(validateInputs);
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('the DOM is ready to be interacted with!');
+});
+
 alert("Hi there!");
 
-// console.log(validateInputs);
 addEvents();
 export { addEvents,
 		 submitTripInfoForm };
