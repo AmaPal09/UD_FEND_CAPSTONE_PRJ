3aadef3d3c86bd915204617a23bd4d895e1991bf
Amarja Palekar <amarjapalekar09@gmail.com>
2022-01-13 21:48:00 -0800

    CHNG: Refactor printToClient

--- src/client/js/printToClient.js
+++ src/client/js/printToClient.js
@@ -1,3 +1,42 @@
+function printTripPlanImages(pixaImgDetails) {
+	if(pixaImgDetails.found) {
+		//TODO: Once loading of file from images is successful, uncomment below lines, remove current attribute set-up
+		document.getElementById("destinationImage").setAttribute("src",
+									pixaImgDetails.imageDetails.webformatURL);
+		/*document.getElementById("destinationImage").setAttribute("src", `./images/defaultImg1.jpg`);*/
+	}
+	else {
+		document.getElementById("destinationImage").setAttribute("src", 												`./images/defaultImg1.jpg`);
+	}
+}
+
+
+function printTripPlanCountdown(tripDetails) {
+	document.getElementById("tripLoc").innerText =
+						`${tripDetails.geonamesDetails.geoDetails.name}, ` +
+						`${tripDetails.geonamesDetails.geoDetails.countryName}`;
+	document.getElementById("tripDaysToGo").innerText = tripDetails.daysToGo;
+}
+
+
+function printTripPlanWeather(weatherDetails) {
+
+	if (weatherDetails.found) {
+		document.getElementById("tempHigh").innerText =
+										`${weatherDetails.weather.high_temp}`;
+		document.getElementById("tempLow").innerText =
+										`${weatherDetails.weather.low_temp}`;
+		document.getElementById("weatherText").innerText =
+							`${weatherDetails.weather.weather.description} `;
+		const weatherIconSrc= `https://www.weatherbit.io/static/img/icons/${							weatherDetails.weather.weather.icon}.png`;
+		console.log(weatherIconSrc);
+		document.getElementById("weatherIcon").setAttribute("src", 																weatherIconSrc);
+	}
+}
+
+
+
+
 /*
 * printTripDetails FUNCTION
 * @description: Prints details of the trip received from the server to
@@ -9,35 +48,15 @@ function printTripDetails(tripDetails) {
 
 	console.log("Enter printTripDetails()");
 	console.log(tripDetails);
-	document.getElementById("tripLoc").innerText =
-						`${tripDetails.geonamesDetails.geoDetails.name}, ` +
-						`${tripDetails.geonamesDetails.geoDetails.countryName}`;
-	document.getElementById("tripDaysToGo").innerText = tripDetails.daysToGo;
+	// document.getElementById("tripLoc").innerText =
+	// 					`${tripDetails.geonamesDetails.geoDetails.name}, ` +
+	// 					`${tripDetails.geonamesDetails.geoDetails.countryName}`;
+	// document.getElementById("tripDaysToGo").innerText = tripDetails.daysToGo;
 
-	if (tripDetails.weatherbitDetails.found) {
-		document.getElementById("tempHigh").innerText =
-						`${tripDetails.weatherbitDetails.weather.high_temp}`;
-		document.getElementById("tempLow").innerText =
-						`${tripDetails.weatherbitDetails.weather.low_temp}`;
-		document.getElementById("weatherText").innerText =
-				`${tripDetails.weatherbitDetails.weather.weather.description} `;
-		const weatherIconSrc= `https://www.weatherbit.io/static/img/icons/${tripDetails.weatherbitDetails.weather.weather.icon}.png`
-		console.log(weatherIconSrc);
-		document.getElementById("weatherIcon").setAttribute("src", weatherIconSrc);
-	}
+	printTripPlanImages(tripDetails.pixabayDetails);
+	printTripPlanCountdown(tripDetails);
+	printTripPlanWeather(tripDetails.weatherbitDetails);
 
-	if(tripDetails.pixabayDetails.found) {
-
-		//TODO: Once loading of file from images is successful, uncomment below lines, remove current attribute set-up
-
-		document.getElementById("destinationImage").setAttribute("src",
-			tripDetails.pixabayDetails.imageDetails.webformatURL);
-
-		/*document.getElementById("destinationImage").setAttribute("src", `./images/defaultImg1.jpg`);*/
-	}
-	else {
-		document.getElementById("destinationImage").setAttribute("src", `./images/defaultImg1.jpg`);
-	}
 }
 
 
