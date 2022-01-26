36f2b625753b2556dcf3bd78fc73057166d197a9
Amarja Palekar <amarjapalekar09@gmail.com>
2022-01-26 15:00:52 -0800

    FEAT: Add service workers to production

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const common = require('./webpack.config.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
 	mode: 'production',
@@ -24,6 +25,7 @@ module.exports = merge(common, {
 	},
 	plugins: [
 		new MiniCssExtractPlugin({filename: "[name].css"}),
+		new WorkboxPlugin.GenerateSW(),
 	],
 	optimization: {
 		minimize: true,
